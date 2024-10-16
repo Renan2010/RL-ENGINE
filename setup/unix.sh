9c441ad7f2a3bd7b159c385391835ed7e3e685a4
Renan Lucas <renanluscad@gmail.com>
2024-10-16 16:23:08 -0300

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -3,10 +3,10 @@
 # REMINDER THAT YOU NEED HAXE INSTALLED PRIOR TO USING THIS
 # https://haxe.org/download
 cd ..
-echo Makking the main haxelib and setuping folder in same time..
+echo "==> Makking the main haxelib and setuping folder in same time.."
 mkdir ~/haxelib && haxelib setup ~/haxelib
-echo Installing dependencies...
-echo This might take a few moments depending on your internet speed.
+echo "==> Installing dependencies..."
+echo "==> This might take a few moments depending on your internet speed."
 haxelib install lime 8.1.2
 haxelib install openfl 9.3.3
 haxelib install flixel 5.6.1
@@ -20,4 +20,4 @@ haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c0
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6bb6df66562b3f24c62f4c5bba14a7
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666
-echo Finished!
+echo "==> Finished"
